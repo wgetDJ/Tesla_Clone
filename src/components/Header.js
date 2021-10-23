571fc3bdf603b12248d8b83390ec9e84c6952cfc
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2021-10-23 07:52:08 +0530

    feat: sidemenu boxshadow

--- src/components/Header.js
+++ src/components/Header.js
@@ -167,6 +167,7 @@ const SideMenu = styled.div`
     list-style: none;
     width: 300px;
     text-align: left;
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.16);
 
     transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
 
